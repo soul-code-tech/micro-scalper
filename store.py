d5590bb15d1ec175a7bacbe1c2d38d1a6159d071
soul-code-tech <225708441+soul-code-tech@users.noreply.github.com>
2025-10-01 14:25:29 +0700

    Delete store.py

--- store.py
+++
@@ -1,9 +0,0 @@
-class Cache:
-    def __init__(self):
-        self._d = {}
-    def get(self, k, default=None):
-        return self._d.get(k, default)
-    def set(self, k, v):
-        self._d[k] = v
-
-cache = Cache()
