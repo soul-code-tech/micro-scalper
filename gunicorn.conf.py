4d3f9c6f66b25cca4fb25fa57edb225d7f1eb7ca
soul-code-tech <225708441+soul-code-tech@users.noreply.github.com>
2025-10-02 10:44:56 +0700

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,8 @@
+bind = "0.0.0.0:10000"
+workers = 2
+worker_class = "uvicorn.workers.UvicornWorker"
+worker_connections = 1000
+keepalive = 5
+max_requests = 1000
+max_requests_jitter = 50
+preload_app = True
