14986e1503658ea1480ca1a336af7e77e538967d
soul-code-tech <225708441+soul-code-tech@users.noreply.github.com>
2025-10-04 08:18:12 +0700

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,7 @@
-bind = "0.0.0.0:10000"
+# gunicorn.conf.py
+import os
+
+bind = f"0.0.0.0:{os.getenv('PORT', '10000')}"
 workers = 2
 worker_class = "uvicorn.workers.UvicornWorker"
 worker_connections = 1000
