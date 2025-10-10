75db7618032ada5b9f1f8e2e412589d28adcb150
soul-code-tech <225708441+soul-code-tech@users.noreply.github.com>
2025-10-10 11:07:51 +0700

    Delete %20jesse-config/jesse-config directory

--- %20jesse-config/jesse-config/routes.py
+++
@@ -1,9 +0,0 @@
-from jesse.config import config
-from strategies.micro_ml import MicroML
-
-routes = [
-    ('BingX', 'DOGE-USDT', '5m', MicroML),
-    ('BingX', 'LTC-USDT', '5m', MicroML),
-    ('BingX', 'SHIB-USDT', '5m', MicroML),
-    ('BingX', 'SUI-USDT', '5m', MicroML),
-]
